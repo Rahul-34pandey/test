0e6a26c46da5fe0701dc5ed58e7ac1ba8dd822ed
Rahul-34pandey <rahulkumar.pandey014181@rec.ac.in>
2024-10-25 10:55:02 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+ void main()
+ {
+ 	int r;
+ 	float area;
+ 	printf("enter value of radius");
+ 	scanf("%d",&r);
+ 	area=3.14*pow(r,2);
+ 	printf("area=%f",area);
+ }
